599601ddfd518aeca2f748ab773b3171b5109bf7
villaclara <villaclara1231@gmail.com>
2024-04-23 00:08:20 +0300

    Add project files.

---
+++ testpublish.wpf/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace testpublish.wpf
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public string VersionText { get; set; } = "";
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			//VersionText = Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "0.0.0.0";
+			VersionTextBlock.Text = "2.0.0";
+		}
+	}
+}
